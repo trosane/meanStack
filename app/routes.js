7dc1484f88f88518b02a5520ac77bb23b7f1f1b5
trosane <trosane@uw.edu>
2016-02-06 12:43:36 -0800

    local auth for signin/login

--- app/routes.js
+++ app/routes.js
@@ -5,42 +5,6 @@ var bcrypt = require('bcrypt-nodejs')
         
         // backend routes =================
         
-        app.get('/api/users', function(req, res) {
-            // uses mongoose to get all users from the database
-            User.find(function(err, users) {
-                //if there is an error retrieving, send an error
-                if (err)
-                    res.send(err);
-                
-                res.json(users); // return all users in JSON format
-            });
-        });
-        
-        // route to handle registering a new user
-        
-        // app.post('/newUser', function(req, res, next) {
-            
-        //     var password = req.body.password;
-            
-        //     var newUser = new User({
-        //         email: req.body.email,
-        //         password: password,
-        //         displayName: req.body.displayName
-        //     });
-            
-        //     newUser.password = newUser.generateHash(password);
-            
-                       
-        //     newUser.save(function(err) {
-        //         if(err) {
-        //             res.send(err);
-        //         } else {
-        //             res.redirect('/');
-        //      }
-            
-        //     });
-        // });
-        
         // LOCAL AUTHENTICATION FOR REGISTERING A NEW USER
         app.post('/newUser', passport.authenticate('local-register', {
             successRedirect : '/success',
